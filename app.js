6832b1065065374df1d86a1c46312b7d82999ed7
MantashaNoyela <mantashanoyela4@gmail.com>
2026-06-02 02:49:55 -0700

    Add dark mode toggle feature

--- app.js
+++ app.js
@@ -35,3 +35,15 @@ form.addEventListener("submit", (event) => {
 
     form.reset();
 });
+
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeBtn.textContent = "Light Mode";
+    } else {
+        darkModeBtn.textContent = "Dark Mode";
+    }
+});
